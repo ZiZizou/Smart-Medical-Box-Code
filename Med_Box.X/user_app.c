9fb6f40aa01c552c47893064da4a01c807011996
rishabh9898 <rishabh.ruhela@ucalgary.ca>
2021-04-06 00:15:30 +0400

    ---added stuff---

--- Med_Box.X/user_app.c
+++ Med_Box.X/user_app.c
@@ -50,7 +50,7 @@ Variable names shall start with "UserApp_<type>" and be declared as static.
 /**********************************************************************************************************************
 Function Definitions
 **********************************************************************************************************************/
-static u8 UserApp_au8sinTable[]=
+u8 G_au8UserAppsinTable[] = 
 {
 0x80,0x83,0x86,0x89,0x8c,0x8f,0x92,0x95,0x98,0x9b,0x9e,0xa2,0xa5,0xa7,0xaa,0xad,
 0xb0,0xb3,0xb6,0xb9,0xbc,0xbe,0xc1,0xc4,0xc6,0xc9,0xcb,0xce,0xd0,0xd3,0xd5,0xd7,
@@ -67,11 +67,57 @@ static u8 UserApp_au8sinTable[]=
 0x00,0x00,0x00,0x00,0x01,0x01,0x01,0x02,0x02,0x03,0x04,0x05,0x05,0x06,0x07,0x09,
 0x0a,0x0b,0x0c,0x0e,0x0f,0x11,0x12,0x14,0x15,0x17,0x19,0x1b,0x1d,0x1f,0x21,0x23,
 0x25,0x28,0x2a,0x2c,0x2f,0x31,0x34,0x36,0x39,0x3b,0x3e,0x41,0x43,0x46,0x49,0x4c,
-0x4f,0x52,0x55,0x58,0x5a,0x5d,0x61,0x64,0x67,0x6a,0x6d,0x70,0x73,0x76,0x79,0x7c,
+0x4f,0x52,0x55,0x58,0x5a,0x5d,0x61,0x64,0x67,0x6a,0x6d,0x70,0x73,0x76,0x79,0x7c   
 };
 /*--------------------------------------------------------------------------------------------------------------------*/
 /*! @publicsection */                                                                                            
 /*--------------------------------------------------------------------------------------------------------------------*/
+/*
+ * 24-Hour Military Format Clock
+ * 
+ * Days:
+ *  - Monday:       0
+ *  - Tuesday:      1
+ *  - Wednesday:    2
+ *  - Thursday:     3
+ *  - Friday:       4
+ *  - Saturday:     5
+ *  - Sunday:       6
+ * 
+ */
+typedef struct {
+    u8 day;
+    u8 hour;
+    u8 minute;
+    u8 second;
+} Clock;
+
+/*
+ * bool days = [Mon, Tue, Wed, Thu, Fri, Sat, Sun]
+ * 
+ */
+typedef struct {
+    int numOfPills;
+    int numOfPillsToTake;
+    u8 days[7];
+    u8 hour;
+    u8 minute;
+    u8 second;
+} Box;
+
+typedef struct {
+    u8 bitMask;
+    u8 state;
+    bool flashing;
+} LED;
+
+typedef struct {
+    u8 bitMask;
+    u8 state;
+    u32 pressedTimer;
+    u32 releasedTimer;
+    bool pressed;
+} Button;
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 /*! @protectedsection */                                                                                            
@@ -94,25 +140,44 @@ Promises:
 - NONE
 
 */
+/* Internal Clock to keep track of Day and Time */
+static Clock clock;
 
-void TimeCheck(u8* u8H, u8* u8M, u8* u8S, u8* u8D){
-    *u8S+=1;
-    if(*u8S>=60){
-        *u8S-=60;
-        *u8M+=1;
-    }
-    if(*u8M>=60){
-        *u8M-=60;
-        *u8H+=1;
-    }
-    if(*u8H>=24){
-        *u8H=0;
-        *u8D*=2;
-        if(*u8D==0b10000000){
-            *u8D=0b00000001;
-        }
-    }
-}
+
+/* Boxes */
+static Box Box1;
+static Box Box2;
+static Box Box3;
+static Box Box4;
+
+
+/* LEDs */
+static LED Led1;
+static LED Led2;
+static LED Led3;
+static LED Led4;
+
+
+/* Buttons */
+static Button Btn1;
+static Button Btn2;
+static Button Btn3;
+static Button Btn4;
+
+
+static Box Boxes[4];
+static LED Leds[4];
+static Button Buttons[4];
+
+
+/* Variable for which alarms are currently active
+ * 0: Off
+ * 1: On (Solid - Dispense Pill)
+ * 2: On (Flashing - Refill Box) 
+ */
+static u8 alarms[] = {0, 0, 0, 0};
+
+
 
 /*!--------------------------------------------------------------------------------------------------------------------
  * @fn void LED(void)
@@ -133,35 +198,7 @@ Promises:
 
 */
 
-void LED(u8 BoxChoice, u8 Mode){
-    if(BoxChoice==1){
-        PORTA = 0x01;
-    }
-    else if(BoxChoice==2){
-        PORTA = 0x02;
-    }
-    else if(BoxChoice==3){
-        PORTA = 0x04;
-    }
-    else{
-        PORTA = 0x08;
-    }
-}
 
-void ALARM(u8 BoxChoice,u8 Mode)
-{
-    //if pill is over
-    static u8 i=0;
-    for(i=0;i<255;i++)
-    {
-        DAC1DATL=UserApp_au8sinTable[i]; // figuring out pic7 , so usong this so far.
-        __delay_ms(1000);// found the timer module a bit complicated
-        if(Mode==0)
-        {
-           break; 
-        }
-    }
-}
 
 /*!--------------------------------------------------------------------------------------------------------------------
  *  * @fn void UserAppInitialize(void)
@@ -178,14 +215,153 @@ Promises:
 - NONE
 
 */
-void UserAppInitialize(void)
-{
+void UserAppInitialize(void) {
 
+    /*-------  Timer  -------*/
+    DAC1DATL= 0;
     T0CON0 = 0x90;
     T0CON1 = 0x54;
-    DAC1DATL= 0;
 
-} /* end UserAppInitialize() */
+
+    // TODO: Add code for importing data from SD Card CSV file
+
+
+    /*-------  Real Time Clock -------*/
+
+    // * Manually set by user at the time of Programming using the CSV
+
+    // Monday 8:05:30 PM
+    clock = (Clock){
+        .day = 0,
+        .hour = 20,
+        .minute = 5,
+        .second = 30
+    };
+
+
+    /*-------  Boxes  -------*/
+
+    Box1 = (Box){
+        .numOfPills = 5,
+        .numOfPillsToTake = 1,
+        .hour = 20,
+        .minute = 5,
+        .second = 40
+    };
+
+    Box2 = (Box){
+        .numOfPills = 10,
+        .numOfPillsToTake = 1,
+        .hour = 9,
+        .minute = 0,
+        .second = 30
+    };
+
+    Box3 = (Box){
+        .numOfPills = 20,
+        .numOfPillsToTake = 1,
+        .hour = 12,
+        .minute = 30,
+        .second = 0
+    };
+
+    Box4 = (Box){
+        .numOfPills = 50,
+        .numOfPillsToTake = 50,
+        .hour = 20,
+        .minute = 5,
+        .second = 35
+    };
+
+    u8 Box1Days[] = {1, 0, 0, 1, 0, 0, 0}; // Mon, Thu
+    u8 Box2Days[] = {0, 0, 0, 0, 1, 0, 0}; // Tue, Fri
+    u8 Box3Days[] = {0, 0, 0, 0, 0, 1, 0}; // Sat
+    u8 Box4Days[] = {1, 0, 1, 0, 1, 0, 0}; // Monday, Wed, Fri
+
+    memcpy(Box1.days, Box1Days, 7);
+    memcpy(Box2.days, Box2Days, 7);
+    memcpy(Box3.days, Box3Days, 7);
+    memcpy(Box4.days, Box4Days, 7);
+
+    /*-------  LEDs  -------*/
+
+    Led1 = (LED){
+        .bitMask = 0b00000001,
+        .state = 0,
+        .flashing = false
+    };
+
+    Led2 = (LED){
+        .bitMask = 0b00000010,
+        .state = 0,
+        .flashing = false
+    };
+
+    Led3 = (LED){
+        .bitMask = 0b00000100,
+        .state = 0,
+        .flashing = false
+    };
+
+    Led4 = (LED){
+        .bitMask = 0b00001000,
+        .state = 0,
+        .flashing = false
+    };
+
+
+    /*-------  Buttons  -------*/
+
+    Btn1 = (Button){
+        .bitMask = 0b00000001,
+        .state = 0,
+        .pressedTimer = 0,
+        .releasedTimer = 0,
+        .pressed = false
+    };
+
+    Btn2 = (Button){
+        .bitMask = 0b00000010,
+        .state = 0,
+        .pressedTimer = 0,
+        .releasedTimer = 0,
+        .pressed = false
+    };
+
+    Btn3 = (Button){
+        .bitMask = 0b00000100,
+        .state = 0,
+        .pressedTimer = 0,
+        .releasedTimer = 0,
+        .pressed = false
+    };
+
+    Btn4 = (Button){
+        .bitMask = 0b00001000,
+        .state = 0,
+        .pressedTimer = 0,
+        .releasedTimer = 0,
+        .pressed = false
+    };
+
+
+    Boxes[0] = Box1;
+    Boxes[1] = Box2;
+    Boxes[2] = Box3;
+    Boxes[3] = Box4;
+
+    Leds[0] = Led1;
+    Leds[1] = Led2;
+    Leds[2] = Led3;
+    Leds[3] = Led4;
+
+    Buttons[0] = Btn1;
+    Buttons[1] = Btn2;
+    Buttons[2] = Btn3;
+    Buttons[3] = Btn4;
+
+}
+ /* end UserAppInitialize() */
 
 /*--------------------------------------------------------------------
 void TimeXus(INPUT_PARAMETER_)
@@ -198,16 +374,45 @@ Promises:
 - TMR0IF cleared
 - Timer0 enabled
 */
-void TimeXus(u16 u16Time)
-{
-/* OPTIONAL: range check and handle edge cases */
-    T0CON0 &= 0x7F;/* Disable the timer during config */
-    u16 u16MicroSeconds = 0xFFFF - u16Time;
-    TMR0H = (u8)(u16MicroSeconds>>8); /* Preload TMR0H and TMR0L based on u16TimeXus */
-    TMR0L = (u8)(u16MicroSeconds&0x00FF);
-    PIR3 &= 0x7F;/* Clear TMR0IF and enable Timer 0 */       
+void TimeXus(u16 u16Time) {
+    // Fosc/4(16Mhz), Async, Prescaler = 1:16
+    T0CON1 = 0b01010100;
+
+    // Disable the timer during config
+    T0CON0 &= 0x7F;
+
+    // Preload TMR0L and TMR0H based on u16TimeXus
+    u16 u16Complement = 0xFFFF - u16Time;
+
+    TMR0L = (u8) (u16Complement & 0x00FF);
+    TMR0H = (u8) (u16Complement >> 8);
+
+    // Clear TMR0IF
+    PIR3 &= 0x7F;
+
+    // Enable the Timer
     T0CON0 |= 0x80;
-} /* end TimeXus () */
+}
+
+void TimeXms(u16 u16Time) {
+    // Fosc/4(16Mhz), Async, Prescaler = 1:16384
+    T0CON1 = 0b01011110;
+
+    // Disable the timer during config
+    T0CON0 &= 0x7F;
+
+    // Preload TMR0L and TMR0H based on u16TimeXus
+    u16 u16Complement = 0xFFFF - u16Time + 0x0001;
+
+    TMR0H = (u8) (u16Complement >> 8);
+    TMR0L = (u8) (u16Complement & 0x00FF);
+
+    // Clear TMR0IF
+    PIR3 &= 0x7F;
+
+    // Enable the Timer
+    T0CON0 |= 0x80;
+}
 
 /*!----------------------------------------------------------------------------------------------------------------------
 @fn void UserAppRun(void)
@@ -221,31 +426,202 @@ Promises:
 - 
 
 */
+void UpdateClock() {
+    static u16 millisecondCounter = 0;
+
+    if (millisecondCounter >= 1000) { /* 1 second has passed */
+        clock.second += 1;
+        millisecondCounter = 0;
+        if (clock.second == 60) {
+            clock.second = 0;
+            clock.minute += 1;
+            if (clock.minute == 60) {
+                clock.minute = 0;
+                clock.hour += 1;
+                if (clock.hour == 24) {
+                    clock.hour = 0;
+                    //need to add code for updating days
+                    clock.day = clock.day>>1;
+                    if(clock.day==0){//check if we right shifted Sunday into zero or not
+                        clock.day = 0b01000000;
+                    }//next day after Sunday is Monday so set to that
+                }
+            }
+        }
+    }
+    millisecondCounter++;
+}
+
+/*
+ * Makes an LED either Solid or Flashing
+ @Requires:
+    - BoxChoice: Which LED to turn on
+    - flashing: Whether the LED should be flashing or solid
+     
+ */
+void ToggleLED(u8 ledChoice, bool flashing) {
+    if (!flashing) {
+        Leds[ledChoice].state = 1;
+    } else {
+        Leds[ledChoice].flashing = true;
+    }
+}
+
+/*
+ * This method will render all 4 of the LEDs according to the 'Leds' array values
+ * Either Off, Solid, or Blinking
+ */
+void RenderLEDs() {
+    u8 leds = PORTA & 0b11110000;
+    static u16 flashingCounter = 0;
+    flashingCounter++;
+
+    for (int i = 0; i < 4; i++) {
+        if (Leds[i].flashing) {
+            if (flashingCounter >= 250) {
+                Leds[i].state ^= 0x01;
+            }
+        }
+        if (Leds[i].state) {
+            leds |= Leds[i].bitMask;
+        }
+    }
+
+    if (flashingCounter >= 250) {
+        flashingCounter = 0;
+    }
+
+    LATA = leds;
+}
+
+/*
+ * Don't use this function directly. This is a helper method.
+ */
+void AddButtonListener(Button* btn, int pressDuration, void (*callback)(), u8 boxChoice) {
+
+    if (btn->pressed) {
+
+        if ((PORTB & btn->bitMask) == 0x00) { // Button either released or bounced
+            btn->releasedTimer++;
+            if (btn->releasedTimer > 10) { // Button released for 10ms
+                btn->pressed = false;
+            }
+        }
+
+        btn->pressedTimer += 1;
+
+        if (btn->pressedTimer >= pressDuration) { // How long before registers as a button press
+            btn->pressed = false;
+            callback(boxChoice);
+        }
+
+    } else if ((PORTB & btn->bitMask) != btn->state && (PORTB & btn->bitMask) == btn->bitMask) {
+        btn->pressed = true;
+        btn->pressedTimer = 0;
+        btn->releasedTimer = 0;
+    }
+
+    btn->state = PORTB & btn->bitMask;
+}
+
+
+void ButtonPress(u8 btnChoice, int pressDuration, void (*callback)()) {
+    AddButtonListener(&Buttons[btnChoice], pressDuration, callback, btnChoice);
+}
+
+void StartAlarm(u8 boxChoice, u8 mode) {
+    
+    u8 index=0;
+    u32 i=0;
+    if (mode == 1) { 
+        
+        // Dispense Pill Alarm
+      while(i<4000000) // ~ approx 10 seconds alarm 
+        {
+            DAC1DATL=G_au8UserAppsinTable[index];
+            index+=4;
+            TimeXus(11);
+            i++;  
+        }
+        i=0;
+        index=0;
+  
+        alarms[boxChoice] = 1;
+        ToggleLED(boxChoice, false);
+
+    } else if (mode == 2) { // Refill Box Alarm
+
+        alarms[boxChoice] = 2;
+        ToggleLED(boxChoice, true);
+        while(i<4000000)
+        {
+            DAC1DATL=G_au8UserAppsinTable[index];
+            index++;
+            TimeXus(11);
+            i++;  
+        }
+        i=0;
+        index=0;
+
+    }
+
+    // TODO: Add code for Alarming speaker
+}
+
+void EndAlarm(u8 boxChoice) {
+
+    if (alarms[boxChoice] == 1) {
+
+        Boxes[boxChoice].numOfPills -= Boxes[boxChoice].numOfPillsToTake;
+
+        if (Boxes[boxChoice].numOfPills <= 0) { // Box is empty
+            StartAlarm(boxChoice, 2); // Create new alarm for Box Refill and return
+            return;
+        }
+
+    } else if (alarms[boxChoice] == 2) {
+        // TODO: Reset numOfPills from CSV
+        Boxes[boxChoice].numOfPills = 20;
+    }
+
+    alarms[boxChoice] = 0;
+    Leds[boxChoice].state = 0;
+    Leds[boxChoice].flashing = false;
+    // TODO: Turn off Speaker
+
+}
+
+
 void UserAppRun(void)
 {
-    
-    //initalize medicine time data here
-    static u8 u8Hour = 17;//enter start time details
-    static u8 u8Minutes =  34;//enter start time details
-    static u8 u8Seconds = 0;//enter start time details
-    static u8 u8days = 0b01111111;//bits showing which day the current day is. Bit 8 is unused and bit 7 is Sunday
-    static u8 u8PillIndex = 0;//get current time by accessing all the time variables
-    static u8 u8A_user_dat[2][7] = {//box number, set initial total pills, pills left, pills to be taken, hour, minute, day
-        {3,50,48,2,17,35,0b01001000}
-    };//sample user data
-    if(((u8A_user_dat[u8PillIndex][6]&u8days)!=0)&&(u8A_user_dat[u8PillIndex][4]==u8Hour)&&(u8A_user_dat[u8PillIndex][5]==u8Minutes)){//check current time against the current pill day and time
-        LED(u8A_user_dat[u8PillIndex][0]);//Test command//call corresponding LED in the right mode
-       // ALARM(  ,)                          //call alarm in the right mode
-                                            //call  to ensure time increments are not missed
-                                            //keep doing the above till snooze button is pressed
+
+    UpdateClock();
+
+
+    // Check if its time for taking a pill in any of the boxes:
+    for (int i = 0; i < 4; i++) {
+
+        if (Boxes[i].days[clock.day] == 1 &&
+                Boxes[i].hour == clock.hour &&
+                Boxes[i].minute == clock.minute &&
+                Boxes[i].second == clock.second)
+        {
+            StartAlarm(i, 1); // Start the Alarm for Dispensing Pill
+
+        }
     }
-    //call refill function and receive return value - return function is being worked on by Atharva
-    //send return value of refill to LED and speaker function
-    TimeCheck(&u8Hour, &u8Minutes, &u8Seconds, &u8days);
-    //end 
-} /* end UserAppRun */
 
+    for (u8 i = 0; i < 4; i++) {
+        if (alarms[i] == 1) {
+            ButtonPress(i, 50, EndAlarm); // Short Press - 50ms
+        } else if (alarms[i] == 2) {
+            ButtonPress(i, 2000, EndAlarm); // Long Press - 2000ms
+        }
+    }
 
+    RenderLEDs();
+
+}
 
 /*------------------------------------------------------------------------------------------------------------------*/
 /*! @privatesection */                                                                                            
